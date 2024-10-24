ce775ac6f0ec0de7e37af132966377472461891e
Magnus0155 <magnuseeg1@gmail.com>
2024-10-24 12:03:14 +0200

    message

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+console.log('Hello world!');
+
+// Variables
+
+// let variable saving the number 27 on the name age
+let age=27;
+
+// chaining method:
+console.log("min alder er:", age);
+// template litteral:
+console.log(`Min alder er ${age}`);
+
+console.log(typeof age);
+
+// const variablesaving the string Magnus on the variablename name.
+const name = "Magnus";
+console.log("Mitt navn er:", name);
+console.log(typeof name);
+
+// const variable saving the booleantrue on the name utsPresent
+const isPresent = true;
+console.log("Er tilstede?", isPresent);
+console.log(typeof isPresent);
+
+const firstName="Magnus";
+
+const lastName="Eeg";
+
+const fullName= firstName+ " " + lastName;
+console.log ("fullName variable viser:", fullName);
